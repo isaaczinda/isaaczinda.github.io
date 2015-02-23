bcf59fc62f2c4ddbcb30912eb9c74b8a601ba519
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2015-02-22 19:23:06 -0800

    update PID to metro look

--- PID/js/main.js
+++ PID/js/main.js
@@ -15,6 +15,11 @@ var PConstant = 1; //make it positive and large
 var IConstant = -.1; //make it negative and small
 var DConstant = -1; //make it negitive and large
 
+$(document).ready(function()
+{
+	$('#myModal').modal('show');
+});
+
 //resize the canvas when the window size changes and on load
 (function($, viewport)
 {
