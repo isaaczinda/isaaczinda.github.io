910965c8cc4cd53ab6c8490508ddf016d97a3ca2
Isaac Zinda <isaaczinda@Isaacs-MacBook-Pro.local>
2016-06-30 23:54:05 -0700

    finish navbar start masonry

--- Home/js/app.js
+++ Home/js/app.js
@@ -1,1 +1,13 @@
-$(document).foundation()
+$(document).ready(function () {
+  var $grid = $('.image-container').imagesLoaded( function()
+  {
+    console.log("images loaded");
+    // init Masonry after all images have loaded
+    $grid.masonry({
+      columnWidth: '.grid-sizer',
+      // do not use .grid-sizer in layout
+      itemSelector: '.image-item',
+      percentPosition: true
+    });
+  });
+});
